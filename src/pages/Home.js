caf7395abb635525488dd7821bd8a960e5bb62be
Mohamed Muaaz <muaazmfm.16@cse.mrt.ac.lk>
2022-11-28 10:05:45 +0530

    Home page banner implementation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
+import { Box } from '@mui/material'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+    </Box>
   )
 }
 
